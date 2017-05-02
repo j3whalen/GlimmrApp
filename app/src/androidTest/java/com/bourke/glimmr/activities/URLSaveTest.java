0db6122702451470ced657d4d111c3f1e0b48ad7
Jeff Turner <jeff.thomas.turner@gmail.com>
2017-05-02 16:48:39 -0400

    added string resources

--- app/src/androidTest/java/com/bourke/glimmr/activities/URLSaveTest.java
+++ app/src/androidTest/java/com/bourke/glimmr/activities/URLSaveTest.java
@@ -5,28 +5,24 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 import com.bourke.glimmr.R;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -40,27 +36,27 @@ public class URLSaveTest {
     @Test
     public void uRLSaveTest() {
         ViewInteraction textView = onView(
-allOf(withId(R.id.textNotNow), withText("or, just browse some photos…"), isDisplayed()));
+                allOf(withId(R.id.textNotNow), withText("or, just browse some photos…"), isDisplayed()));
         textView.perform(click());
-        
+
         ViewInteraction actionMenuItemView = onView(
-allOf(withId(R.id.menu_search), withContentDescription("Search"), isDisplayed()));
+                allOf(withId(R.id.menu_search), withContentDescription("Search"), isDisplayed()));
         actionMenuItemView.perform(click());
-        
+
         ViewInteraction searchAutoComplete = onView(
-allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
-withParent(allOf(withClassName(is("android.widget.LinearLayout")),
-withParent(withClassName(is("android.widget.LinearLayout"))))),
-isDisplayed()));
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
+                        withParent(allOf(withClassName(is("android.widget.LinearLayout")),
+                                withParent(withClassName(is("android.widget.LinearLayout"))))),
+                        isDisplayed()));
         searchAutoComplete.perform(replaceText("tiger"), closeSoftKeyboard());
-        
+
         ViewInteraction searchAutoComplete2 = onView(
-allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("tiger"),
-withParent(allOf(withClassName(is("android.widget.LinearLayout")),
-withParent(withClassName(is("android.widget.LinearLayout"))))),
-isDisplayed()));
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("tiger"),
+                        withParent(allOf(withClassName(is("android.widget.LinearLayout")),
+                                withParent(withClassName(is("android.widget.LinearLayout"))))),
+                        isDisplayed()));
         searchAutoComplete2.perform(pressImeActionButton());
-        
-        }
 
     }
+
+}
