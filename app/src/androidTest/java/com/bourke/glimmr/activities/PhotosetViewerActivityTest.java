6cfabafa0b3dd86599026198ca3fe81806d6ab25
Josh Whalen <j3whalen@student.bridgew.edu>
2017-05-03 17:58:28 -0400

    Added toasts when API sends back Photos and also a hypothetical test

---
+++ app/src/androidTest/java/com/bourke/glimmr/activities/PhotosetViewerActivityTest.java
@@ -0,0 +1,22 @@
+/*
+package java.com.bourke.glimmr.activities;
+
+import com.bourke.glimmr.fragments.photoset.PhotosetGridFragment;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+___________________________________
+  Created by Josh on 5/3/2017.
+-----------------------------------
+public class PhotosetViewerActivityTest {
+    @Test
+    public void initViewPager() throws Exception {
+        ///if(PhotosetGridFragment.newInstance() == PreviousPhotos){
+                  Fail();
+
+    }
+
+}
+*/
